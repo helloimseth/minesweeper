51189ca532e8d0e6a8311d9b5cbd53a40a3a38fd
Seth Hamlin <helloimseth@gmail.com>
2015-01-26 11:37:33 -0500

    add further structure in tile, player, and game.

--- mines.rb
+++ mines.rb
@@ -20,6 +20,10 @@ class Board
   end
 
   def pass_input_to_tile
+    #could call reveal
+  end
+
+  def reveal_board_on_game_over
 
   end
 
@@ -28,8 +32,9 @@ end
 
 
 class Tile
+  attr_reader :neighbors
 
-  DELTAS #constant of move locations 
+  DELTAS #constant of move locations
 
   def initialize
     @neighbors = #store local nodes
@@ -37,21 +42,37 @@ class Tile
     @bomb #finish this
     @pos #finish this
     @board #pass board
+    @revealed = false
   end
 
-  def
+  def inspect
+    "Pos: #{@pos}\nBomb?: #{@bomb}\nFlagged? #{@flagged}\nRevealed? #{@revealed}\n
+    Neighbor B.C. #{neighbor_bomb_count}"
+  end
+
+  def add_neighbor
+    #add a single neighbor
+  end
+
+  def add_neighbors
+    #loop that does all the deltas
+  end
 
   def receive_board_input
 
   end
 
   def flag_as_bomb
-
+    @flagged = !@flagged
   end
 
 
   def reveal
     #count local bombs
+  end
+
+  def neighbor_bomb_count
+    nil
   end
 
 
@@ -60,13 +81,27 @@ end
 
 class Player
 
+  def initialize
 
+  end
+
+  def get_input
+  # either reveal or flag and a position
+  end
 end
 
 
 
 class Game
 
+  def initialize
+
+  end
+
+  def play
+  #get input, and handle the input
+  #we'll need an input loop to continue getting player input
+  end
 
 end
 
